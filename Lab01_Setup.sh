5a1453e8884a60a1f627e88473fa1b25fe770829
Joff Thyer <joff@blackhillsinfosec.com>
2025-09-04 11:31:10 -0400

    lab01 checked and cleared

--- Lab01_Setup.sh
+++ Lab01_Setup.sh
@@ -2,10 +2,10 @@ if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
     echo "You must run this script using 'source ${0}'"
     exit 1
 fi
-LAB="lab16"
-LDIR="./016_Fundamentals"
+LAB="lab01"
+LDIR="./Lab01_PhishingDetectionModel"
 conda deactivate
-`conda env list | grep -q $LAB` && (echo "Removing Existing $LAB16 conda env" && conda env remove -q --yes -n $LAB)
+`conda env list | grep -q $LAB` && (echo "Removing Existing $LAB conda env" && conda env remove -q --yes -n $LAB)
 conda create --yes -n $LAB python=3.12
 conda activate $LAB
 pip install --upgrade poetry
