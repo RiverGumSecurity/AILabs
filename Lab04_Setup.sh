d2b6a1dd8b7742353372d620480782010292f211
Derek Banks <derek@blackhillsinfosec.com>
2025-09-29 18:07:33 -0400

    adding Lab04 setup script again

---
+++ Lab04_Setup.sh
@@ -0,0 +1,19 @@
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+    echo "You must run this script using 'source ${0}'"
+    exit 1
+fi
+LAB="lab04"
+LDIR="./Lab04_CyberThreatIntelTracking"
+conda deactivate
+`conda env list | grep -q $LAB` && (echo "Removing Existing $LAB conda env" && conda env remove -q --yes -n $LAB)
+conda create --yes -n $LAB python=3.13
+conda activate $LAB
+pip install --upgrade poetry
+cd $LDIR
+poetry install --no-root
+git clone https://github.com/deruke/prism.git
+cd prism
+echo "########################################################"
+echo " $LAB Setup is complete."
+echo " More instructions in this directory from here. "
+echo "########################################################"
